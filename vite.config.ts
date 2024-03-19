46763b2408f0e4f52ede34e08eefe635f6dc4a12
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-19 13:32:08 +0100

    chore: update  test setup for typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+/// <reference types="vitest" />
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
   build: {

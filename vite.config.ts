113f15038994edce2184a3f69f7b576dead830be
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-19 12:08:44 +0100

    chore: prepare library for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'React-components',
+      fileName: (format) => `index.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  }
 })

b58e12c105100556e9b2bc4e32795c7628278fd3
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-20 14:16:18 +0100

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,41 @@
 /// <reference types="vitest" />
-import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import path from 'path'
 
-export default defineConfig({
-  plugins: [react(), dts()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'React-components',
-      fileName: (format) => `index.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      root: path.resolve(__dirname, 'src'),
+      build: {
+        outDir: path.resolve(__dirname, 'dist')
       }
-    },
-    sourcemap: true,
-    emptyOutDir: true
+    }
+  } else {
+    return {
+      plugins: [react(), dts(), cssInjectedByJsPlugin()],
+      build: {
+        lib: {
+          entry: path.resolve(__dirname, 'index.ts'),
+          name: 'React-components',
+          fileName: (format) => `index.${format}.js`
+        },
+        rollupOptions: {
+          input: path.resolve(__dirname, 'index.ts'),
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM'
+            }
+          }
+        },
+        sourcemap: true,
+        emptyOutDir: true
+      }
+    }
   }
 })

8cbaa36757a0a11ea1ff8ead1c4732cc05c8474c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-21 01:10:39 +0100

    chore(prettier): add/configure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,7 @@ module.exports = {
     "plugin:react/recommended",
     "airbnb",
     "airbnb/typescript",
+    "plugin:prettier/recommended",
   ],
   overrides: [
     {
@@ -26,7 +27,7 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "react"],
+  plugins: ["@typescript-eslint", "react", "prettier"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
